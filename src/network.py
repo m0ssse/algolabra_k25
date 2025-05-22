1af3f4b91263f7b0d78f70f5239c14192e542be1
Antti Moisio <moisioa1@gmail.com>
2025-05-22 12:42:02 +0300

    Added unit tests for Network class

--- src/network.py
+++ src/network.py
@@ -1,36 +1,57 @@
 import numpy as np
 
 class Network:
-    def __init__(self, neuron_counts: list, activation_functions: list, weights: list = None, biases: list = None):
+    def __init__(self, neuron_counts: list, activation_functions: list, weights: list = None, biases: list = None, randomise_weights: bool = True, randomise_biases: bool = True):
         """
         Initialize the network.
 
         Args:
             neuron_counts: The number of neurons in each layer of the network
+            activation_functions: The activation function for each layer
+            weights: A list of arrays containing to the weight matrix between each layer of neurons. Only the first n-1 matrices will be considered where n is the number of layers
+            biases: A list of bias vectors for each layer
+            randomise_weights: If no weights are provided and this flag is True, then the weights will be initially randomised. Otherwise, the weights are set to zero
+            randomise_bias: If not biases are provided and this flag is True, then the biases will be initially randomised. Otherwise, they will be set to zero
         """
         self.layer_size = neuron_counts
         self.activation_functions = activation_functions
         if weights is None:
             self.weights = []
-            for i in range(1, len(neuron_counts)):
-                n, m = self.layer_size[i-1], self.layer_size[i]
-                self.weights.append(np.random.rand(n, m))
+            if randomise_weights:
+                for i in range(1, len(neuron_counts)):
+                    n, m = self.layer_size[i-1], self.layer_size[i]
+                    self.weights.append(np.random.rand(m, n))
+            else:
+                for i in range(1, len(neuron_counts)):
+                    n, m = self.layer_size[i-1], self.layer_size[i]
+                    self.weights.append(np.zeros((m, n)))
         else:
             self.weights = weights
         if biases is None:
             self.biases = []
-            for i in range(1, len(neuron_counts)):
-                m = self.layer_size[i]
-                self.biases.append(np.random.rand(m, 1))
+            if randomise_biases:
+                for i in range(1, len(neuron_counts)):
+                    m = self.layer_size[i]
+                    self.biases.append(np.random.rand(m, 1))
+            else:
+                for i in range(1, len(neuron_counts)):
+                    m = self.layer_size[i]
+                    self.biases.append(np.zeros((m, 1)))                    
+
+
         else:
             self.biases = biases
 
     def feed_forward(self, x: np.ndarray) -> tuple:
         """
-        This method computes the output of the network with the given output. The method returns a tuple containing the weighted sum, activation and the derivation of the activation in each layer
+        This method computes the output of the network with the given output. The method returns a tuple containing the weighted sum, activation and the derivation of the activation in each layer.
+        The derivative is returned as it is required for backpropagation
 
         Args:
-            x: A numpy array corresponding to the input vector¨
+            x: A numpy array corresponding to the input vector
+
+        Output:
+            A tuple whose element is a list containing the activations of each layer and the second element is a list containing the derivatives of the activation of each neuron
 
         """
         activations = [x]
@@ -63,10 +84,6 @@ class Network:
 
 
 if __name__=="__main__":
-    w1 = np.ones((2, 2))
-    w2 = np.eye(2)
-    b = np.zeros((2, 1))
-    my_network = Network([2, 2, 2], [Network.sigmoid, Network.identity], [w2, w1], [b, b])
-    x = np.array([[3], [1]])
-    a, d = my_network.feed_forward(x)
-    print(a)
+    mynw = Network([2, 3, 4], [Network.identity, Network.identity])
+    a, _  = mynw.feed_forward(np.array([[1], [1]]))
+    print(a[-1])
--- src/tests/network_test.py
+++ src/tests/network_test.py
@@ -1,7 +1,87 @@
 import unittest
-import network
+import network as nw
 import numpy as np
 
 class TestNetwork(unittest.TestCase):
-    def setUp(self):
-        self.network = network.Network([2, 2], [network.identity, network.identity], [])
+    def test_predetermined_weights(self):
+        network_size = np.random.randint(3, 10)
+        layer_sizes = [np.random.randint(2, 10) for _ in range(network_size)]
+        weights = [np.random.randint(0, 4, (layer_sizes[i], layer_sizes[i-1])) for i in range(1, network_size)]
+        network = nw.Network(layer_sizes, [nw.Network.identity for _ in range(network_size-1)], weights)
+        for w1, w2 in zip(weights, network.weights):
+            self.assertTrue((w1==w2).all())
+        x = np.ones((layer_sizes[0], 1))
+        network.feed_forward(x) #We pass a vector through the network to verify that the resulting network has consistent sizes
+
+    def test_randomised_weights(self):
+        network_size = np.random.randint(3, 10)
+        layer_sizes = [np.random.randint(2, 10) for _ in range(network_size)]
+        network = nw.Network(layer_sizes, [nw.Network.identity for _ in range(network_size-1)])
+        for w in network.weights:
+            self.assertTrue((w<np.ones(w.shape)).all())
+            self.assertTrue((w>np.zeros(w.shape)).all())
+        x = np.ones((layer_sizes[0], 1))
+        network.feed_forward(x)       
+
+    def test_zero_weights(self):
+        network_size = np.random.randint(3, 10)
+        layer_sizes = [np.random.randint(2, 10) for _ in range(network_size)]
+        network = nw.Network(layer_sizes, [nw.Network.identity for _ in range(network_size-1)], randomise_weights=False)
+        for w in network.weights:
+            self.assertTrue((w==np.zeros(w.shape)).all())
+        x = np.ones((layer_sizes[0], 1))
+        network.feed_forward(x)
+
+    def test_predetermined_bias(self):
+        network_size = np.random.randint(3, 10)
+        layer_sizes = [np.random.randint(2, 10) for _ in range(network_size)]
+        biases = [np.random.randint(0, 10, (layer_sizes[i], 1)) for i in range(1, network_size)]
+        network = nw.Network(layer_sizes, [nw.Network.identity for _ in range(network_size-1)], biases=biases)
+        for b1, b2 in zip(biases, network.biases):
+            self.assertTrue((b1==b2).all())
+        x = np.ones((layer_sizes[0], 1))
+        network.feed_forward(x)
+
+    def test_randomised_bias(self):
+        network_size = np.random.randint(3, 10)
+        layer_sizes = [np.random.randint(2, 10) for _ in range(network_size)]
+        network = nw.Network(layer_sizes, [nw.Network.identity for _ in range(network_size-1)])
+        for b in network.biases:
+            self.assertTrue((b<np.ones(b.shape)).all())
+            self.assertTrue((b>np.zeros(b.shape)).all())
+        x = np.ones((layer_sizes[0], 1))
+        network.feed_forward(x)
+
+    def test_zero_bias(self):
+        network_size = np.random.randint(3, 10)
+        layer_sizes = [np.random.randint(2, 10) for _ in range(network_size)]
+        network = nw.Network(layer_sizes, [nw.Network.identity for _ in range(network_size-1)], randomise_biases=False)
+        for b in network.biases:
+            self.assertTrue((b==np.zeros(b.shape)).all())
+        x = np.ones((layer_sizes[0], 1))
+        network.feed_forward(x)
+
+    def test_output_size(self):
+        """The output size should be the same as the number of neurons in the final layer"""
+        network_size = np.random.randint(2, 7)
+        layer_sizes = [np.random.randint(2, 10) for _ in range(network_size)]
+        network = nw.Network(layer_sizes, [nw.Network.identity for _ in range(network_size-1)])
+        x = np.zeros((layer_sizes[0], 1))
+        out, _ = network.feed_forward(x)
+        self.assertEqual(out[-1].shape, (layer_sizes[-1], 1))
+
+    def test_output_with_identity_no_bias(self):
+        """
+        If there are no biases and all activations are identity functions, the network should behave as a matrix product
+        """
+        network_size = np.random.randint(3, 10)
+        layer_sizes = [np.random.randint(2, 10) for _ in range(network_size)]
+        weights = [np.random.randint(0, 4, (layer_sizes[i], layer_sizes[i-1])) for i in range(1, network_size)]
+        network = nw.Network(layer_sizes, [nw.Network.identity for _ in range(network_size-1)], weights, None, False, False)
+        x = np.eye(layer_sizes[0])
+        out, _ = network.feed_forward(x)
+        w_prod = weights[0]
+        for i in range(1, len(weights)):
+            self.assertTrue((w_prod==out[i]).all())
+            w_prod = weights[i]@w_prod
+        self.assertTrue((w_prod==out[-1]).all())
