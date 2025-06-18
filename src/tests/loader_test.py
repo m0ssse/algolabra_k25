4c2abc42b4bcdc994a9d119e4db91a583779a00c
Antti Moisio <moisioa1@gmail.com>
2025-06-18 10:31:33 +0300

    Started rewriting tests

--- src/tests/loader_test.py
+++ src/tests/loader_test.py
@@ -1,28 +1,33 @@
 import unittest
 import mnist_loader as loader
+from pathlib import Path
 import numpy as np
 
 class DataLoaderTest(unittest.TestCase):
     def test_data_set_size(self):
-        images, size, _, _ = loader.MNISTLoader.read_image_data("data/train-images.idx3-ubyte")
+        images, size, _, _ = loader.read_image_data(Path("data/train-images.idx3-ubyte"))
         self.assertEqual(len(images), size)
 
     def test_image_sizes(self):
-        images, _, rows, cols = loader.MNISTLoader.read_image_data("data/train-images.idx3-ubyte")
+        images, _, rows, cols = loader.read_image_data(Path("data/train-images.idx3-ubyte"))
         for im in images:
             self.assertEqual(len(im), rows*cols)
 
     def test_label_amount(self):
-        labels, size = loader.MNISTLoader.read_labels("data/train-labels.idx1-ubyte")
+        labels, size = loader.read_labels(Path("data/train-labels.idx1-ubyte"))
         self.assertEqual(len(labels), size)
 
     def test_image_amount_vs_label_amount(self):
-        _, image_N, _, _ = loader.MNISTLoader.read_image_data("data/train-images.idx3-ubyte")
-        _, label_N = loader.MNISTLoader.read_labels("data/train-labels.idx1-ubyte")
+        _, image_N, _, _ = loader.read_image_data(Path("data/train-images.idx3-ubyte"))
+        _, label_N = loader.read_labels(Path("data/train-labels.idx1-ubyte"))
         self.assertEqual(image_N, label_N)
 
-    def test_label_range(self):
-        labels, _ = loader.MNISTLoader.read_labels("data/train-labels.idx1-ubyte")
+    def labels_have_one_nonzero_element(self):
+        labels, _ = loader.read_labels(Path("data/train-labels.idx1-ubyte"))
         for l in labels:
-            self.assertGreaterEqual(l, 0)
-            self.assertLessEqual(l, 9)
+            self.assertEqual(np.count_nonzero(l), 1)
+
+    def lables_have_correct_shape(self):
+        labels, _ = loader.read_labels(Path("data/train-labels.idx1-ubyte"))
+        for l in labels:
+            self.assertEqual(l.shape, (10, 1))
