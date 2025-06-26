6b8a097603d96bc373cc19a368220ce97f1a43e7
Antti Moisio <moisioa1@gmail.com>
2025-06-26 09:45:09 +0300

    Further UI improvements

--- src/main.py
+++ src/main.py
@@ -3,70 +3,97 @@ import neural_network
 import mnist_loader
 from pathlib import Path
 
-def help():
-    print("Commands: ")
-    print("a: Create a new network")
-    print("t: Train a network")
-    print("s: Show networks")
-    print("x: Exit")
+class NeuralNetworkApplication:
+    def __init__(self):
+        self.train_images, _, _, _ = mnist_loader.read_image_data(Path("data/train-images.idx3-ubyte"))
+        self.train_labels, _ = mnist_loader.read_labels(Path("data/train-labels.idx1-ubyte"))
+        self.test_images, _, _, _ = mnist_loader.read_image_data(Path("data/t10k-images.idx3-ubyte"))
+        self.test_labels, _ = mnist_loader.read_labels(Path("data/t10k-labels.idx1-ubyte"))
 
-def main():
-    train_images, _, _, _ = mnist_loader.read_image_data(Path("data/train-images.idx3-ubyte"))
-    train_labels, _ = mnist_loader.read_labels(Path("data/train-labels.idx1-ubyte"))
-    test_images, _, _, _ = mnist_loader.read_image_data(Path("data/t10k-images.idx3-ubyte"))
-    test_labels, _ = mnist_loader.read_labels(Path("data/t10k-labels.idx1-ubyte"))
+        batch_size = 32
+        self.batches = mnist_loader.make_batches(self.train_images, self.train_labels, batch_size)
 
-    batch_size = 32
-    batches = mnist_loader.make_batches(train_images, train_labels, batch_size)
+        self.networks = []
 
-    networks = []
-    while True:
-        help()
-        command = input()
-        if command=="x":
+    def help(self):
+        print("Commands: ")
+        print("a: Create a new network")
+        print("t: Train a network")
+        print("s: Show networks")
+        print("x: Exit")
+
+    def show_networks(self):
+        for i, network in enumerate(self.networks):
+            print(f"{i+1}: {network}")
+    
+    def add_network(self):
+        hidden_neurons = -1
+        while hidden_neurons<0:
+            try:
+                hidden_neurons = int(input("Choose the number of neurons in the hidden layer or type 0 to go back: "))
+                if hidden_neurons<0:
+                    raise ValueError
+            except ValueError:
+                print("The number of neurons must be a positive integer!")
+            if hidden_neurons==0:
+                return
+        self.networks.append(neural_network.Network(784, hidden_neurons, 10))
+
+    def train_network(self):
+        if not self.networks:
+            print("No networks to train!")
             return
-        if command=="a":
-            hidden_neurons = int(input("How many neurons in the hidden layer? "))
-            networks.append(neural_network.Network(784, hidden_neurons, 10))
-        if command=="s":
-            for i, network in enumerate(networks):
-                print(f"{i+1}: {network}")
-        if command=="t":
-            if not networks:
-                print("No networks to train!")
-                continue
-            print(f"There are {len(networks)} networks available to train.")
-            ind = int(input(f"Choose a network to train: "))
-            if ind<1 or ind>len(networks):
-                print("")
-                continue
-            epochs=-1
-            learn_rate = -1
-            while epochs<0:
-                try:
-                    epochs = int(input("Choose the number of epochs (suitable values should be around 10) or type 0 to cancel: "))
-                    if epochs<0:
-                        raise ValueError
-                except ValueError:
-                    print("The number of epochs must be a positive integer")
-            if epochs==0:
-                continue
+        print(f"There are {len(self.networks)} networks available to train.")
+        ind = -1
+        while ind<0:
+            try:
+                ind = int(input(f"Choose a network to train or type 0 to go back: "))
+                if ind<0 or ind>len(self.networks):
+                    raise ValueError
+            except ValueError:
+                print("Invalid choice!")
+        if ind==0:
+            return
+        epochs=-1
+        learn_rate = -1
+        while epochs<0:
+            try:
+                epochs = int(input("Choose the number of epochs (suitable values should be around 10) or type 0 to cancel: "))
+                if epochs<0:
+                    raise ValueError
+            except ValueError:
+                print("The number of epochs must be a positive integer")
+        if epochs==0:
+            return
 
-            while learn_rate<0:
-                try:
-                    learn_rate = float(input("Choose the learn rate (suitable values should be around 3) or type 0 to cancel: "))
-                    if learn_rate<0:
-                        raise ValueError
-                except ValueError:
-                    print("The learn rate must be a positive number!")
-            if learn_rate==0:
-                continue
-            show_progress = bool(int(input("Would you like to display training progress? (1: show progress, 0: don't show progress) ")))
-            networks[ind-1].train_network(epochs, batches, learn_rate, test_images, test_labels, show_progress)
-            print("Training finished!")
-            print()
+        while learn_rate<0:
+            try:
+                learn_rate = float(input("Choose the learn rate (suitable values should be around 3) or type 0 to cancel: "))
+                if learn_rate<0:
+                    raise ValueError
+            except ValueError:
+                print("The learn rate must be a positive number!")
+        if learn_rate==0:
+            return
+        
+        show_progress = bool(int(input("Would you like to display training progress? (1: show progress, 0: don't show progress) ")))
+        print("Training...")
+        self.networks[ind-1].train_network(epochs, self.batches, learn_rate, self.test_images, self.test_labels, show_progress)
+        print("Training finished!")
+        print()
 
-
+    def run(self):
+        while True:
+            self.help()
+            command = input()
+            if command=="x":
+                return
+            if command=="a":
+                self.add_network()
+            if command=="s":
+                self.show_networks()
+            if command=="t":
+                self.train_network()
 
 if __name__=="__main__":
-    main()
+    NeuralNetworkApplication().run()
